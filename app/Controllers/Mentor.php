5d3bcb418a27cb6efea9d3aa32ce93e21ebeda3b
NIKOLA SIMIC <nikolasimicva@hotmail.com>
2022-03-03 11:33:21 +0100

    v.9.6

--- app/Controllers/Mentor.php
+++ app/Controllers/Mentor.php
@@ -55,7 +55,6 @@ class Mentor extends BaseController
         
 
 
-        
        $mentorUpit = $this->user->builder()->where('id', user_id())->get()->getResultArray()[0];
        $data['mentor'] = $mentorUpit;
        $mentorId = $mentorUpit['id'];
