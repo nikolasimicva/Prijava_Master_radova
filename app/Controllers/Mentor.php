617f57091153322dc6c0193b55da562b7cb62047
NIKOLA SIMIC <nikolasimicva@hotmail.com>
2022-03-04 11:14:49 +0100

    v.9.6

--- app/Controllers/Mentor.php
+++ app/Controllers/Mentor.php
@@ -49,7 +49,9 @@ class Mentor extends BaseController
 
     // Mentor azurira vec postojecu prijavu odredjenog studenta
     public function prijava_azuriraj($id)
-    {    
+    {  
+        
+        
        $mentorUpit = $this->user->builder()->where('id', user_id())->get()->getResultArray()[0];
        $data['mentor'] = $mentorUpit;
        $mentorId = $mentorUpit['id'];
