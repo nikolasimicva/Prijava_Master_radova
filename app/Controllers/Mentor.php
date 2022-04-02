65e2b9bb5c3569d6abd389f49bb573aadfd2fc21
NIKOLA SIMIC <nikolasimicva@hotmail.com>
2022-04-02 10:42:51 +0200

    v.9.8

--- app/Controllers/Mentor.php
+++ app/Controllers/Mentor.php
@@ -54,6 +54,9 @@ class Mentor extends BaseController
        $data['mentor'] = $mentorUpit;
        $mentorId = $mentorUpit['id'];
     
+
+
+       
         // prijava
         $prijavaUpit = $this->prijavaModel->builder()->where('id', $id)
         ->get()->getResultArray()[0];
