6fac1c8c0fad50e53a69ba6e00a07ca2997ed74f
Nikola Simic <nikolasimicva@hotmail.com>
2023-10-07 18:02:52 +0200

    v 1.192

--- app/Views/dokumenta/obrazlozenje_teme.php
+++ app/Views/dokumenta/obrazlozenje_teme.php
@@ -270,6 +270,7 @@
                             </form>
                         </div>
                     </div>
+                    
                     <div class="row">
                         <div class="col-md-6">
                             <button type="submit" class="button-green">
