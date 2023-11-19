e408d0f210be9657a1c75981837a535554dd728f
Nikola Simic <nikolasimicva@hotmail.com>
2023-11-19 09:17:20 +0100

    v 1.194

--- app/Views/dokumenta/obrazlozenje_teme.php
+++ app/Views/dokumenta/obrazlozenje_teme.php
@@ -96,7 +96,6 @@
                                         </label>
                                     </div>
                                 </div>
-
                                 <div class="form-group row mb-0 ml-5">
                                     <label for="place_1"
                                         class="col-0 col-form-label ml-1 text-md-left"><b>Кандидат</b></label>
