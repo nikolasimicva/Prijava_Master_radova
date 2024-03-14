c1dabc2247470886f58ab750d480cde726156471
Nikola Simic <nikolasimicva@hotmail.com>
2024-03-14 16:15:32 +0100

    v 2.22

--- app/Views/dokumenta/obrazlozenje_teme.php
+++ app/Views/dokumenta/obrazlozenje_teme.php
@@ -101,7 +101,7 @@
                                                 class="form-control underline"
                                                 name="username"></u>
                                     </div>
-                                    
+
                                 </div>
                                 <div class="form-group row mb-2 ml-5">
                                     <label for="place_1"
@@ -127,7 +127,6 @@
                                             ЗА СТУДИЈЕ II СТЕПЕНА АКАДЕМСКИХ
                                             СТУДИЈА</b></label>
                                 </div>
-
                                 <div class="form-group row mb-0 ml-5">
                                     <label for="place_1"
                                         class="col-0 col-form-label ml-1 text-md-left"><b>Предмет:
