392637decc208ab7d4e1a02a040d6913b3a9f147
Nikola Simic <nikolasimicva@hotmail.com>
2024-04-08 20:00:57 +0200

    v 2.45

--- app/Views/rukovodilac/prijava_azuriraj.php
+++ app/Views/rukovodilac/prijava_azuriraj.php
@@ -153,7 +153,6 @@ $this->section('content');
                 </div>
             </div>
             <button type="submit" class="btn btn-primary btn-block">Ажурирајте пријаву</button>
-
             <br/>
             <p></p>
             <?php
