f2f3b1cc14ed59f762d9390a482f81abf47356bf
Nikola Simic <nikolasimicva@hotmail.com>
2024-04-29 11:12:13 +0200

    v 2.47

--- app/Views/stsluzba/izbor_studenta.php
+++ app/Views/stsluzba/izbor_studenta.php
@@ -123,6 +123,7 @@ $this->section('content');
             echo anchor('stsluzba/prijava_azuriraj/'.$row['id'], 'измени', ['class' => 'btn btn-outline-dark ml-2']); 
             ?>
         </td>
+        
         <td class="text-center"> 
             <?php
             echo anchor('stsluzba/obrazlozenje_azuriraj/'.$row['id_student'], 'измени', ['class' => 'btn btn-outline-dark ml-2']);
