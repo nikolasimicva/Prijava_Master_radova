cc63b31d6d379c363614aedaebeb771fb2f974c0
Nikola Simic <nikolasimicva@hotmail.com>
2024-05-11 12:27:20 +0200

    v 2.47

--- app/Views/stsluzba/obrazlozenje_azuriraj.php
+++ app/Views/stsluzba/obrazlozenje_azuriraj.php
@@ -28,7 +28,7 @@ $this->section('content');
                 </div>
                 
                 <br>
-                
+
                 <div class="form-group">
                     <label for="indeks">Број индекса</label>
                     <input type="text"
@@ -37,6 +37,7 @@ $this->section('content');
                         <?php $prijava_indeks = old('indeks') ?? $prijava['indeks'] ?>
                         value="<?= $prijava_indeks ?>">
                 </div>
+                
                 <br>
                 <div class="form-group">
                     <label for="modul">Модул</label>
