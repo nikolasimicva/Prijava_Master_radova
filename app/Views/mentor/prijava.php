b8a19f4d87939a6589bb68132c1927a6515a74e8
NIkola Simic <nikolasimicva@hotmail.com>
2024-10-19 19:44:09 +0200

    v 2.253

--- app/Views/mentor/prijava.php
+++ app/Views/mentor/prijava.php
@@ -52,7 +52,6 @@ $this->section('content');
                             selected<?php endif; ?>><?= $m['username'] ?></option>
                         <?php endforeach; ?>
                     </select>
-
                 </div>
 
                 <div class="form-check">
