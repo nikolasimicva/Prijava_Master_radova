fcf7f9124a7056b7a34673418f9b4b7323f49e03
NIkola Simic <nikolasimicva@hotmail.com>
2024-11-11 09:32:49 +0200

    v 2.254

--- app/Views/komisija/menu.php
+++ app/Views/komisija/menu.php
@@ -23,7 +23,6 @@ $link = [
 
             <div class="sb-sidenav-menu-heading">Статус пријаве
             </div>
-
             <div class="sb-sidenav-menu-heading">Операције
             </div>
             <a class="nav-link" href="index.html">
