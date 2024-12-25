c56b965671c94c6e9ea8ef0be0be257f0605fdee
NIkola Simic <nikolasimicva@hotmail.com>
2024-12-25 09:38:08 +0200

    v 2.256

--- app/Views/student/pdf_prijava.php
+++ app/Views/student/pdf_prijava.php
@@ -229,11 +229,8 @@ $this->section('content');
 function printDiv(divName) {
     var printContents = document.getElementById(divName).innerHTML;
     var originalContents = document.body.innerHTML;
-
     document.body.innerHTML = printContents;
-
     window.print();
-
     document.body.innerHTML = originalContents;
 }
 </script>
