8cc357e7312799100eab212a7d5ecbceae010cca
NIkola Simic <nikolasimicva@hotmail.com>
2025-01-24 09:14:22 +0200

    v 2.261

--- app/Views/stsluzba/odluka.php
+++ app/Views/stsluzba/odluka.php
@@ -20,17 +20,8 @@ $link = [
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
- 
-
     <meta name="viewport"
-
- 
-
         content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
- 
-
     <meta name="description" content="" />
 
  
