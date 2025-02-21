7236481fbc8cbed1bd8d823e666d890190e803c4
NIkola Simic <nikolasimicva@hotmail.com>
2025-02-21 06:20:48 +0200

    v 2.263

--- app/Views/mentor/obrazlozenje_azuriraj.php
+++ app/Views/mentor/obrazlozenje_azuriraj.php
@@ -16,7 +16,6 @@ $this->section('content');
                 <?= csrf_field() ?>
 
 
-
                 <input type="hidden" name="id_student" hidden value="<?= $id_student ?>">
                 <input name="tema_id" hidden value="<?= $tema['id'] ?>">
                 <div class="form-group">
