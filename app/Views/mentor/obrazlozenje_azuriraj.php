087acd4ed0b9a9477421f4f46b761a175514e32b
NIkola Simic <nikolasimicva@hotmail.com>
2025-02-22 09:52:08 +0200

    v 2.263

--- app/Views/mentor/obrazlozenje_azuriraj.php
+++ app/Views/mentor/obrazlozenje_azuriraj.php
@@ -14,7 +14,6 @@ $this->section('content');
             <div class="col-sm-6 col-xs-12">
                 <?= view('Myth\Auth\Views\_message_block') ?>
                 <?= csrf_field() ?>
-
                 <input type="hidden" name="id_student" hidden value="<?= $id_student ?>">
                 <input name="tema_id" hidden value="<?= $tema['id'] ?>">
                 <div class="form-group">
