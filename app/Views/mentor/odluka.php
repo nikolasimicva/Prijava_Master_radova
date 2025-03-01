89fd8fece39bf04a72e2abbd201c1c4c5e01d72a
NIkola Simic <nikolasimicva@hotmail.com>
2025-03-01 09:50:27 +0200

    v 2.264

--- app/Views/mentor/odluka.php
+++ app/Views/mentor/odluka.php
@@ -85,7 +85,6 @@ $this->section('content');
 
 
 
-
         <td class="text-center">        
             <?php
             echo anchor('mentor/izbor_studenta/', 'измени', ['class' => 'btn btn-outline-dark ml-2']);
