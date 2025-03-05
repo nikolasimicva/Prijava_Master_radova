215c089f7439356b43378c9483e87cfc3e7aa7ba
NIkola Simic <nikolasimicva@hotmail.com>
2025-03-05 08:12:56 +0200

    v 2.264

--- app/Views/mentor/prijava_azuriraj.php
+++ app/Views/mentor/prijava_azuriraj.php
@@ -78,6 +78,12 @@ $this->section('content');
                         value="<?= $prijava_predmet ?>">
                 </div>
                 <br>
+
+
+
+
+
+                
                 <div class="form-group">
                     <label for="naslov_sr">Наслов мастер рада на српском језику (написан
                         ћирилицом)</label>
