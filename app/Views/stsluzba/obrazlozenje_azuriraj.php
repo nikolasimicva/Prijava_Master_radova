c0f85415183f4776ad9c04ac99662266cfd2a195
NIkola Simic <nikolasimicva@hotmail.com>
2025-04-22 22:04:09 +0200

    v 2.267

--- app/Views/stsluzba/obrazlozenje_azuriraj.php
+++ app/Views/stsluzba/obrazlozenje_azuriraj.php
@@ -35,6 +35,12 @@ $this->section('content');
                         <?php $prijava_indeks = old('indeks') ?? $prijava['indeks'] ?>
                         value="<?= $prijava_indeks ?>">
                 </div>
+
+
+
+
+
+                
                 <br>
                 <div class="form-group">
                     <label for="modul">Модул</label>
