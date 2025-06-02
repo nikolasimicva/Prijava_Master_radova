166d4ba5ea6e088770aeedc8fc99313ef0a93164
NIkola Simic <nikolasimicva@hotmail.com>
2025-06-02 20:06:50 +0200

    v 2.27

--- app/Views/stsluzba/odluka.php
+++ app/Views/stsluzba/odluka.php
@@ -130,27 +130,20 @@ $this->section('content');
         echo "<td class='text-center'>" . $row['id'] . "</td>";
         echo "<td class='text-center'>" . $row['username'] . "</td>";
         ?>
-        
         <td class="text-center">
-
             <?php
             echo anchor('stsluzba/prijava_azuriraj/'.$row['id'], 'измени', ['class' => 'btn btn-outline-dark ml-2']);
             ?>
-
         </td>
         <td class="text-center">
-
             <?php
             echo anchor('stsluzba/obrazlozenje_azuriraj/'.$row['id_student'], 'измени', ['class' => 'btn btn-outline-dark ml-2']);
             ?>
-
         </td>
         <td class="text-center">  
-
             <?php
             echo anchor('stsluzba/biografija_azuriraj/'.$row['id_student'], 'измени', ['class' => 'btn btn-outline-dark ml-2']);
-            ?>
-            
+            ?>   
         </td>
                 <td>
                 <div class="form-group">
