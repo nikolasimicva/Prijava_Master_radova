eaa59078b0f9c46bbdda389bce0c9b42c79f5076
NIkola Simic <nikolasimicva@hotmail.com>
2025-06-04 19:24:24 +0200

    v 2.28

--- app/Views/stsluzba/prijava_azuriraj.php
+++ app/Views/stsluzba/prijava_azuriraj.php
@@ -112,15 +112,11 @@ $this->section('content');
                     <select
                         class="form-control <?php if (session('errors.clan2')) : ?>is-invalid<?php endif ?> mt-3"
                         id="clan2" name="clan2">
-                        
                         <?php foreach ($mentori as $clan) : ?>
-
                         <option value="<?= $clan['id'] ?>"
                             <?php if ($clan['id'] == $clan2['id']) : ?>
-                            selected<?php endif; ?>><?= $clan['username'] ?></option>
-                        
+                            selected<?php endif; ?>><?= $clan['username'] ?></option> 
                         <?php endforeach; ?>
-
                     </select>
                     <br>
                     <label for="clan3">Трећи члан комисије</label>
