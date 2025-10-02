4dd22ff3c45d88b1fde732323d00ffd06be3469b
NIkola Simic <nikolasimicva@hotmail.com>
2025-10-02 21:14:54 +0200

    v 2.32

--- app/Views/mentor/izbor_studenta.php
+++ app/Views/mentor/izbor_studenta.php
@@ -94,6 +94,14 @@ $link = [
         <table class="table table-striped custab">
         <thead>
             <tr>
+
+
+
+
+
+
+
+            
                 <th>ID пријаве</th>
                 <th>Име и презиме</th>
                 <th class="text-center">Пријава</th>
