900924f6195ff4fbd359e70a3888528495af50bc
NIkola Simic <nikolasimicva@hotmail.com>
2026-01-10 10:56:01 +0100

    v 2.33

--- app/Views/stsluzba/odluka.php
+++ app/Views/stsluzba/odluka.php
@@ -6,6 +6,7 @@ $link = [
     'Насловна' => 'stsluzba/home',
 ];
 ?>
+
 <!-- tabela iz bootstrap teme -->
 <head>
     <meta charset="utf-8" />
@@ -151,6 +152,7 @@ $this->section('content');
                     </div>
                 </div>
             </main>
+
  <!-- tabele iz bootstrapa -->
 <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
@@ -160,6 +162,7 @@ $this->section('content');
         crossorigin="anonymous"></script>
     <script src="js/datatables-simple-demo.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    
     <?php 
         $this->endSection(); 
     ?>
