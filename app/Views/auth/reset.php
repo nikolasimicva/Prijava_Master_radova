894d9d42d95741e50da5006b35561b70474cf651
NIkola Simic <nikolasimicva@hotmail.com>
2026-03-08 16:31:08 +0100

    v 2.33

--- app/Views/auth/reset.php
+++ app/Views/auth/reset.php
@@ -40,6 +40,7 @@ $this->section('content');
                                 <?= session('errors.email') ?>
                             </div>
                         </div>
+                        
                         <br>
                         <div class="form-group">
                             <label for="password"><?= lang('Auth.newPassword') ?></label>
